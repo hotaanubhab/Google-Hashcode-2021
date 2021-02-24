eb88457df6985f61746220274c5cecd4770c4496
Abhishek Kujur <59303347+NewCyberGypsy@users.noreply.github.com>
2021-02-24 20:34:16 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -15,7 +15,7 @@
 #define pll pair<ll, ll>
 #define mod 1000000007
 #define inf 1000000000000000001;
-#define all(c) c.begin(), c.end()
+#define all(v) v.begin(), v.end()
 #define mp(x, y) make_pair(x, y)
 #define mem(a, val) memset(a, val, sizeof(a))
 #define eb emplace_back
