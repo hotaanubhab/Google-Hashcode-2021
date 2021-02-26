199ea452429a7a2ef83981829a7ad36736bcbcde
Änubhab <hota000anubhab@gmail.com>
2021-02-26 14:06:41 +0530

    Running Code

--- B/trial.cpp
+++ B/trial.cpp
@@ -25,9 +25,9 @@
 
 using namespace std;
 
-int in_array(string value, string *array)
+int in_array(string value, string array[],int size)
 {
-    int size = (*array).size();
+  
     for (int i = 0; i < size; i++)
     {
         if (value == array[i])
@@ -41,71 +41,79 @@ int in_array(string value, string *array)
 
 int main()
 {
-	int D;
-	int I;
-	int S;
-	int V;
-	int F;
-	cin>>D>>I>>S>>V>>F;
-			int B[S];
-			int E[S];
-			int time[S];
-		string n[S];
-		vvi path;
-	for(int i=0;i<S;i++)
-	{
-		cin>>B[i];
-		cin>>E[i];
-		cin>>n[i];
-		cin>>time[i];
-	}
-	for(int i=0;i<V;i++)
-	{
-		int p;
-		cin>>p;
-		vector<int> temp_path;
-		for(int j=0;j<p;j++)
-		{
-			
-			string k;
-			cin>>k;
-			int temp_route  = in_array(k, n);
-			temp_path.push_back(temp_route);
-		}
-		path.push_back(temp_path);
-	}
-
-  cout<<I<<endl;
-  int tt = 0,ti=0,tc=0,ta=1;
+  int D;
+  int I;
+  int S;
+  int V;
+  int F;
+  cin>>D>>I>>S>>V>>F;
+      int B[S];
+      int E[S];
+      int time[S];
+    string n[S];
+    int pri[V];
+    vvi path;
+    for(int i=0;i<V;i++)
+  {
+    pri[i]=0;
+  }
+  for(int i=0;i<S;i++)
+  {
+    cin>>B[i];
+    cin>>E[i];
+    cin>>n[i];
+    cin>>time[i];
+  }
+  for(int i=0;i<V;i++)
+  {
+    int p;
+    cin>>p;
+    vector<int> temp_path;
+    for(int j=0;j<p;j++)
+    {
+      
+      string k;
+      cin>>k;
+      int temp_route  = in_array(k, n,S);
+      pri[i]=pri[i]+time[temp_route];
+      temp_path.push_back(temp_route);
+    }
+    path.push_back(temp_path);
+  }
+  
+for(int i=0;i<V;i++)
+  {
+    cout<<pri[i]<<endl;
+  }
+  /*cout<<I<<endl;
+  int tt = 0,ti=0,tc=0;
   for(int i=0;i<I;i++)
   {
-  	cout<<i<<endl;
-  	for(int j=S-1;j>=0;j--)
-  	{
-  		if(E[j]==i){tc++;}
-  	}
-  	cout<<tc<<endl;
-  	tc=0;
-  	for(int j=S-1;j>=0;j--)
-  	{
-  		if(E[j]==i){cout<<n[j]<<" "<<ta+1<<endl;
-      if(ta==1)ta=0;
-      else ta = 1;}
-  	}
+    cout<<i<<endl;
+    for(int j=0;j<S;j++)
+    {
+      if(E[j]==i){tc++;}
+    }
+    cout<<tc<<endl;
+    tc=0;
+    for(int j=0;j<S;j++)
+    {
+      if(E[j]==i){cout<<n[j]<<" "<<1<<endl;}
+    }
 
-  }
+  }*/
   /*while(tt<D)
   {
-  	cout<<ti<<endl;
-  	for(int j=0;j<S && tt<D;j++)
-  	{
-  		if(id[j].s==ti){tc++;tt++;}
-  	}
-  	cout<<tc<<endl;
-  	for(int j=0;j<tc;j++)
-  	{
-  		if(id[j].s==ti)cout<<n[j]<<" "<<1<<endl;
-  	}
+    cout<<ti<<endl;
+    for(int j=0;j<S && tt<D;j++)
+    {
+      if(id[j].s==ti){tc++;tt++;}
+    }
+    cout<<tc<<endl;
+    for(int j=0;j<tc;j++)
+    {
+      if(id[j].s==ti)cout<<n[j]<<" "<<1<<endl;
+    }
   }*/
     return 0;
 }
